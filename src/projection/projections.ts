7e92eb2c0bbd5d983eb6b4536aec9d7dd736220e
niuee <vntchang@gmail.com>
2024-01-04 16:54:10 +0800

    Fix the wrong calculation for clipping in orthographic projection

--- src/projection/projections.ts
+++ src/projection/projections.ts
@@ -5,7 +5,7 @@ export function mercatorProjection(interestPoint: GeoCoord, centerLongitude: num
     const r = 6371000;
     const latitude = interestPoint.latitude * Math.PI / 180;
     const longitude = interestPoint.longitude * Math.PI / 180;
-    let x = r * (longitude - centerLongitude * Math.PI / 180);
+    let x = r * normalizeAngleMinusPiToPi(longitude - centerLongitude * Math.PI / 180);
     let y = r * Math.log(Math.tan(Math.PI / 4 + latitude / 2));
     return {x: x, y: y};
 }
@@ -23,7 +23,7 @@ export function orthoProjection(interestPoint: GeoCoord, origin: GeoCoord): {cli
     const longitude = interestPoint.longitude * Math.PI / 180;
     const x = r * Math.cos(latitude) * Math.sin(longitude - origin.longitude * Math.PI / 180);
     const y = r * (Math.cos(origin.latitude * Math.PI / 180) * Math.sin(latitude) - Math.sin(origin.latitude * Math.PI / 180) * Math.cos(latitude) * Math.cos(longitude - origin.longitude * Math.PI / 180));
-    const clipped = Math.cos(Math.sin(origin.latitude * Math.PI / 180) * Math.sin(latitude) + Math.cos(origin.latitude * Math.PI / 180) * Math.cos(latitude) * Math.cos(longitude - origin.longitude * Math.PI / 180));
+    const clipped = Math.sin(origin.latitude * Math.PI / 180) * Math.sin(latitude) + Math.cos(origin.latitude * Math.PI / 180) * Math.cos(latitude) * Math.cos(longitude - origin.longitude * Math.PI / 180);
 
     return {clipped: clipped < 0, coord: {x: x, y: y}};
 }
@@ -35,4 +35,14 @@ export function inverseOrthoProjection(interestPoint: Point, origin: GeoCoord): 
     const latitude = Math.asin(Math.cos(c) * Math.sin(origin.latitude * Math.PI / 180) + (interestPoint.y * Math.sin(c) * Math.cos(origin.latitude * Math.PI / 180)) / rho) * 180 / Math.PI;
     const longitude = origin.longitude + Math.atan2(interestPoint.x * Math.sin(c), rho * Math.cos(c)*Math.cos(origin.latitude * Math.PI / 180) - interestPoint.y * Math.sin(c) * Math.sin(origin.latitude * Math.PI / 180)) * 180 / Math.PI;
     return {latitude: latitude, longitude: longitude};
+}
+
+function normalizeAngleMinusPiToPi(angle: number): number {
+    // Reduce the angle
+    angle = angle % (Math.PI * 2);
+
+    // Force it to be in the range -π to π
+    angle = (angle + 3 * Math.PI) % (2 * Math.PI) - Math.PI;
+
+    return angle;
 }
